ba788bb25fc9b77da4def628b3ef9818e1c93ecf
abhinotnow <abhinavmucharla27@gmail.com>
2025-09-23 01:45:37 +0530

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,5 @@
+name = input("Enter your name: \n")
+age = int(input("Enter your age: \n"))
+like = bool(input("Do you like python? \n"))
+
+print(f"Hi, my name is {name}. I am {age} years old. It is {like} that I like Python.")
